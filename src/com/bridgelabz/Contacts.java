d59ab635c4ecd84a25d58d34825435cbbdcfa5d7
bhure01 <dattatribhure@gmail.com>
2022-01-12 14:52:29 +0530

    Added: created contact class with variables and addressbook class

---
+++ src/com/bridgelabz/Contacts.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Contacts {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String zipCode;
+    private String state;
+    private String phoneNo;
+    private String eMail;
+}

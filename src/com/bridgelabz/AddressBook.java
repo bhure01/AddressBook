795adc73aa155a6bd10faf4642b9cae37eb2875d
bhure01 <dattatribhure@gmail.com>
2022-01-16 14:39:56 +0530

    Modified: deleteing contact

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
@@ -8,8 +9,9 @@ public class AddressBook {
 
     Contacts contacts = new Contacts();     // created object for Contacts class for getter setter methods
 
-    //creating method to add a new contact into addree book
+    ArrayList<Contacts> list = new ArrayList<Contacts>();
 
+    //creating method to add a new contact into address book
     public Contacts addNewContact() {
 
         System.out.println("Please enter the below contact details");
@@ -38,19 +40,67 @@ public class AddressBook {
         String eMail = sc.next();
         contacts.seteMail(eMail);
 
-        return contacts;
+        list.add(contacts);
     }
 
-    // created method to display the added contact details
+    // UC-2 created method to display the added contact details
     public void showContactDetails(Contacts contacts) {
-        System.out.println("Display Contact Details :- ");
-        System.out.println("First Name :- " +contacts.getFirstName());
-        System.out.println("Last Name :- " +contacts.getLastName());
-        System.out.println("Address :- " + contacts.getAddress());
-        System.out.println("City :- " +contacts.getCity());
-        System.out.println("Zip Code :- " +contacts.getZipCode());
-        System.out.println("State :- " +contacts.getState());
-        System.out.println("Phone Number :- " +contacts.getPhoneNo());
-        System.out.println("Email ID :- " +contacts.geteMail());
+        for (Contacts contact : list) {
+            System.out.println("Display Contact Details :- ");
+            System.out.println("First Name :- " + contacts.getFirstName());
+            System.out.println("Last Name :- " + contacts.getLastName());
+            System.out.println("Address :- " + contacts.getAddress());
+            System.out.println("City :- " + contacts.getCity());
+            System.out.println("Zip Code :- " + contacts.getZipCode());
+            System.out.println("State :- " + contacts.getState());
+            System.out.println("Phone Number :- " + contacts.getPhoneNo());
+            System.out.println("Email ID :- " + contacts.geteMail());
+        }
+    }
+
+    // UC3-Creating method to edit an existing contact
+    public void editContactDetail() {
+        // use first name to edit the contact
+        System.out.println("Enter the first name :- ");
+        String firstName = sc.nextLine();
+
+        // checking for valid user input
+        if (!firstName.equalsIgnoreCase(contacts.getFirstName())) {
+            System.out.println("Entered First Name details is not available in AddressBook");
+        } else {
+            System.out.println("Enter last name :- ");
+            contacts.setLastName(sc.nextLine());
+            System.out.println("Enter Address ;-");
+            contacts.setAddress(sc.nextLine());
+            System.out.println("Enter City :");
+            contacts.setCity(sc.nextLine());
+            System.out.println("Enter ZipCode:");
+            contacts.setZipCode(sc.next());
+            System.out.println("Enter State: ");
+            contacts.setState(sc.next());
+            System.out.println("Enter Phone Number: -");
+            contacts.setPhoneNo(sc.next());
+            System.out.println("Enter mail ID : ");
+            contacts.seteMail(sc.next());
+        }
+    }
+
+    // uc4- created method to delete an existing contact
+    public void deteleContact() {
+        System.out.println("Enter first neme : ");  // use first name of contact to delete
+        String firstName = sc.nextLine();
+        // check for user input with first name whether available or not
+        if(firstName.equalsIgnoreCase(contacts.getFirstName())) {
+            System.out.println("Entered First name is not avaialble in address book");
+        } else {
+            contacts.setFirstName(null);
+            contacts.setLastName(null);
+            contacts.setAddress(null);
+            contacts.setCity(null);
+            contacts.setZipCode(null);
+            contacts.setState(null);
+            contacts.setPhoneNo(null);
+            contacts.seteMail(null);
+        }
     }
 }
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -4,8 +4,40 @@ import  java.util.Scanner;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System problem");
-        AddressBook addressBook = new AddressBook();
+        AddressBook addressBook = new AddressBook();  //object of addressbook class for managing contacts
         Contacts contacts = addressBook.addNewContact();
         addressBook.showContactDetails(contacts);
+        System.out.println("Enter Yes or No");
+
+        int choice;     //variable for users choice
+
+//         // show menu for user input to select choices
+        do {
+            System.out.println("****AddressBook Management****");
+            System.out.println("1.Add New Contact \n 2.Edit Contact Details \n 3.Delete Contact Details" + "\n 4.Display Contact Details \n 5.Exit");
+            System.out.println("Please select the choice: ");
+            choice = addressBook.sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    addressBook.addNewContact(); // adding new contact detail
+                    break;
+                case 2:
+                    addressBook.editContactDetail(); // editing existing contact detail
+                    break;
+                case 3:
+                    addressBook.deteleContact();    // delete contact details from address book
+                    break;
+                case 4:
+                    addressBook.showContactDetails(contacts);
+                    break;
+                case 5:
+                    System.out.println("Thanks for using AddressBook");
+                default:
+                    System.out.println("Please select choice between 1 to 5");
+                    break;
+            }
+        }
+        while(choice != 5);
     }
 }
